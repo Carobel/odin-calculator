b7059a3620abb05e81e97a0a62089234a53d095b
Carobel <cjkusee@gmail.com>
2025-09-14 12:02:55 +0200

    add operate function and operation variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// operation control
+let number1;
+let numer2;
+let operator;
+
+function operate(num1, num2, op) {
+    switch (op) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
 // math operations
 function add (num1, num2) {
     return num1 + num2;
@@ -15,3 +33,4 @@ function divide(num1, num2) {
     return num2 == 0 ? 'Nice try.' : num1 / num2;
 }
 
+console.log(operate(2,8,'+'))
