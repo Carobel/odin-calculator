f800e931bd4083540da54def49dea6848269c867
Carobel <cjkusee@gmail.com>
2025-09-14 20:21:56 +0200

    handle digit entry and display

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // operation control: stored as strings for easier concatenation 
-let number1;
-let numer2;
+let number1 = '';
 let operator;
+let number2 = '';
 
 function operate(num1, num2, op) {
     num1 = Number(num1);
@@ -35,10 +35,38 @@ function divide(num1, num2) {
     return num2 == 0 ? 'Nice try.' : num1 / num2;
 }
 
+// handle digit entry
+function handleDigit(digit) {
+    if (!operator) {
+        number1 += digit;
+        updateDisplay(number1);
+    } else {
+        number2 += digit;
+        updateDisplay(number2);
+    }
+}
+
+// update display
+function updateDisplay(num) {
+    display = document.querySelector('#display');
+    display.textContent = num;
+}
+
 // buttons event listener
 buttonBox = document.querySelector('#button-box');
 buttonBox.addEventListener('click', (event) => {
     const targetId = event.target.id;
+    const digits = ['0','1','2','3','4','5','6','7','8','9'];
+    const operators = ['+','-','*','/'];
+
+    if (digits.includes(targetId)) {
+        console.log('nr', targetId);
+        handleDigit(targetId);
+    } else if (operators.includes(targetId)) {
+        console.log('operator', targetId)
+    } else {
+        console.log('choice not implemented')
+    }
 
     // IF clicked is nr
     // ELSE IF clicked is operant
