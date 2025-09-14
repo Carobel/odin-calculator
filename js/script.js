a05e006e57f87af709a4ea214bcb966ff81b1faa
Carobel <cjkusee@gmail.com>
2025-09-14 11:44:41 +0200

    create math operation functions

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// math operations
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 = num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num2 == 0 ? 'Nice try.' : num1 / num2;
+}
+
